909c487330acc3115acabc70d2c239327d471a65
useryansong <useryansong@gmail.com>
2019-11-11 23:05:08 -0500

    Updates

---
+++ precache-manifest.d4eaed574c15b4a302c629ae1278a326.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d6fa7b2fe2c5a70c8e73b229bf3bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "392b5f95035f6065ac3e",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "1911bf2cd3edf73c3140",
+    "url": "/static/js/2.8a6887ae.chunk.js"
+  },
+  {
+    "revision": "392b5f95035f6065ac3e",
+    "url": "/static/js/main.454f055a.chunk.js"
+  },
+  {
+    "revision": "bb6e324978c6b93739f5",
+    "url": "/static/js/runtime-main.e7b7af1d.js"
+  }
+]);
